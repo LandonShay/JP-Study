108e084203953cf49841b0c0f7360126ef962193
LandonShay <lanishay55@gmail.com>
2025-07-01 16:09:24 -0400

    JP-7 deleted unused deck class

--- JP Dictionary/Models/Deck.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JP_Dictionary.Models
-{
-    public class Deck
-    {
-        public string ProfileName { get; set; } = string.Empty;
-        public string DeckName { get; set; } = string.Empty;
-        public List<StudyWord> Words { get; set; } = new();
-    }
-}
